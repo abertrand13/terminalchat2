cbbf47e3bb6ec269619a79708ee6a2a20c0e9997
Alex Bertrand <alex.bertrand13@gmail.com>
2016-03-25 13:44:35 -0400

    Buncha changes
    User logging - tc2 will tell you when people sign on and off
    Queueing messages - if you're in the middle of typing, tc2 will wait to
    display incoming messages until after you've sent whatever you were
    writing.
    Probably should refactor soon...

--- tc2.js
+++ tc2.js
@@ -10,8 +10,6 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
-var db = new Firebase(process.env.dburl); // make this more secure LOL
-
 keypress(process.stdin);
 var readingInput = false;
 
@@ -21,52 +19,94 @@ var uid;
 console.log("Welcome to Terminal Chat!");
 console.log("Ctrl+s to send a message, Ctrl+q to quit");
 
+// do this after the identify step so you already have a name
+var db = new Firebase(process.env.dburl); // make this more secure LOL
+
 rl.question('Identify Yourself: ', (myName) => {
 	name = myName;
-	uid = name + Math.random();
+
+	uid = db.child('users').push(name).key();
+	
+	// remove our name from the list of users when we disconnect
+	db.child('users').child(uid).onDisconnect().remove();
+
+	// check for those that have joined
+	db.child('users').on('child_added', function(snapshot) {
+		var user = snapshot.val();
+		printInfo("[" + user + " has joined]");
+	});
 });
 
+var queuedMessages = [];
+
 // log received messages
-db.on('child_removed', function(snapshot) {
+db.child('messages').on('child_removed', function(snapshot) {
 	var newMsg = snapshot.val();
+
+	// queue messages that come up while we're in the middle of writing one
+	if(readingInput && newMsg.uid != uid) {
+		queuedMessages.push(newMsg);
+		return;
+	}
+	
 	if(newMsg.uid == uid) {
 		// we sent this message
-		console.log("[delivered]");
-	} else {	
-		console.log(newMsg.name, "::", newMsg.text);
+		printInfo("[delivered]");
+	} else {
+		displayMessage(newMsg);
 	}
 });
 
+db.child('users').on('child_removed', function(snapshot) {
+	var user = snapshot.val();
+	printInfo("[" + user + " has left]");
+});
+
+
 // interrupt for sending messages
 process.stdin.on('keypress', function(ch, key) {
-
+	
 	if(key && key.name == 's' && key.ctrl && !readingInput) {
 		readingInput = true;
-		rl.question(name + ' :: ' , (msg) => {
+		rl.question("\x1b[31m" + name + '\x1b[0m :: ' , (msg) => {
 			// LITERALLY JUST ADDING THIS AND REMOVING IT AND IT KILLS ME
 			// I don't see any major reasons (at the moment) why message delivery would be
 			// unreliable, but I'll have to do some testing
-			db.push({
+			db.child('messages').push({
 				"uid"  : uid,
 				"name" : name,
 				"text" : msg
 			}).remove();
 
-			readingInput = false;	
+			readingInput = false;
+			processMessageQueue();
 		});
 	}
 
-	// ctrl q to quit
-	if(key && key.name == 'q' && key.ctrl) {
-		console.log("Shutting down.");
-		process.exit(0);
-	}
-
-	// also ctrl c, because not having it is confusing
-	if(key && key.name == 'c' && key.ctrl) {
+	// ctrl (q or c) to quit
+	if(key && (key.name == 'q' || key.name == 'c') && key.ctrl) {
 		console.log("Shutting down.");
 		process.exit(0);
 	}
 });
+
+
+function processMessageQueue() {
+	var msg;
+	if(queuedMessages.length > 0) {
+		printInfo("[reading from queue]");
+	}
+	while(msg = queuedMessages.shift()) {
+		displayMessage(msg);
+	}
+}
+
+function displayMessage(newMsg) {	
+	console.log("\x1b[37m" + newMsg.name +  "\x1b[0m :: " + newMsg.text);
+}
+
+function printInfo(msg) {
+	console.log("\x1b[34m" + msg + "\x1b[0m");
+}
 
 process.stdin.setRawMode(true);
